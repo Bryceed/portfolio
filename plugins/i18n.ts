e2eec0491bad2378b6a35836cc7f1cf0c8e70438
Wellington N <wellington.tvd@outlook.com.br>
2023-04-17 08:16:26 -0300

    Add support for English (en-US)

--- plugins/i18n.ts
+++ plugins/i18n.ts
@@ -2,6 +2,7 @@ import { createI18n } from 'vue-i18n'
 import en from '../locales/en.json'
 import es from '../locales/es.json'
 import pt from '../locales/pt-BR.json'
+import fr from '../locales/fr.json'
 
 const datetimeFormats = {
   'en-US': {
@@ -34,11 +35,10 @@ export default defineNuxtPlugin(({ vueApp }) => {
       locale: 'pt-BR',
       fallbackLocale: 'en',
       messages: {
-        en: {
-          datetimeFormats
-        },
+        en,
         es,
         pt,
+        fr
       },
     });
   
