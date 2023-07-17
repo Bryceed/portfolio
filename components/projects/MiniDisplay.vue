168000e23e7088acb31702353ec470e1be4023ed
Wellington N <wellington.tvd@outlook.com.br>
2023-07-16 21:56:49 -0300

    [rollback]: restoring classical design

--- components/projects/MiniDisplay.vue
+++ components/projects/MiniDisplay.vue
@@ -1,23 +1,21 @@
 <template>
     <div class="mini-display-container">
-                    <h2 v-html="$t('html.home.projects.title')"></h2>
-                <div class="mini-display">
-                    <template v-for="project in projects">
-                        <div class="project-item active">
-                            <div class="_bg" :style="{ backgroundImage: 'url(' + project.placeholder + ')' }"></div>
-                            <div class="info-overlay">
-                                <h3>{{ project.title }}</h3>
-                                <p>{{ project.description }}</p>
-                            </div>
-                            <div class="actions">
-                                <CommonButton :link="project.link" :title="$t('html.projects.miniDisplay.button')" :origin="'miniDisplay'"></CommonButton>
-                            </div>
-                        </div>
-                    </template>
+        <h2>Projetos</h2>
+        <div class="mini-display">
+            <template v-for="project in projects">
+                <div class="project-item active">
+                    <div class="_bg" :style="{ backgroundImage: 'url(' + project.placeholder + ')' }"></div>
+                    <div class="info-overlay">
+                        <h3>{{ project.title }}</h3>
+                        <p>{{ project.description }}</p>
+                    </div>
+                    <div class="actions">
+                        <CommonButton :link="project.link" :title="$t('html.projects.miniDisplay.button')" />
+                    </div>
                 </div>
-                <NuxtLink to="/projects" class="btn btn-primary btn-lg btn-block btn-float-right">
-                    <span v-html="$t('html.home.actions[0].label')"></span>
-                </NuxtLink>
+            </template>
+        </div>
+        <NuxtLink to="/projects" class="btn btn-primary btn-lg btn-block btn-float-right">Ver todos os projetos</NuxtLink>
     </div>
 </template>
 
@@ -37,7 +35,7 @@
         position: absolute;
         bottom: 1.5rem;
         right: 25px;
-        z-index: 2;
+        z-index: 999;
         font-weight: bold;
         transition: all .1s ease-in-out;
         background-color: #070707;
@@ -62,7 +60,7 @@
         justify-content: center;
         align-items: center;
         position: relative;
-        width: stretch;
+        width: 100%;
         min-height: calc(33vh + 50px);
         padding: 2rem 0 4rem;
         background: #111;
@@ -78,26 +76,23 @@
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.2), 0 0 20px rgba(0, 0, 0, 0.2);
     }
     .mini-display {
-        display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
-        grid-gap: 2em;
-        grid-auto-flow: row;
-        grid-auto-rows: 200px;
-        justify-content: center;
-        justify-items: center;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-evenly;
         align-items: center;
-        width: 90%;
-        margin-bottom: 2rem;
+        font-size: 0;
+        min-height: calc(33vh + 50px);
     }
 
     .project-item {
         display: flex;
         flex-direction: column;
-        justify-content: flex-start;
+        justify-content: start;
         align-items: start;
-        width: 320px;
-        height: 200px;
-        margin: 1em;
+        width: calc(33vw - 3rem);
+        height: calc(33vw / 1.7);
+        margin: 1rem;
         border-radius: 8px;
         background: #111;
         color: #000;
@@ -109,8 +104,8 @@
         transition: all 0.2s ease-in-out;
         cursor: pointer;
         box-shadow: 0 0 0 5px #222222, 0 0 0 10px #782fee;
-        height: 180px;
-        width: 310px;
+        height: calc(33vw / 1.7 - 20px);
+        width: calc(33vw - 3rem - 20px);
         margin: calc(1rem + 10px);
     }
     html.light .project-item:hover {
@@ -168,19 +163,19 @@
 
     .project-item .info-overlay h3 {
         transition: all 1.5s ease-in;
-        font-size: 2.5em;
+        font-size: 2.5rem;
         font-weight: bold;
-        text-shadow: 0 0 10px rgba(0, 0, 0, 0.2), 0 1px 2px rgba(0, 0, 0, 0.2);
+        text-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
         position: absolute;
-        bottom: 5%;
+        top: 48%;
         transition: all 0.2s ease-out;
-        opacity: 0;
+        text-align: center;
     }
     .project-item:hover .info-overlay h3 {
         transition: all 0.2s ease-in-out;
-        font-size: 1.2rem;
-        bottom: 30%;
-        opacity: 1;
+        font-size: 1.5rem;
+        top: 65%;
+        text-align: left;
     }
     .project-item .info-overlay p {
         font-size: .75rem;
@@ -191,7 +186,7 @@
         opacity: 0;
         color: rgba(255, 255, 255, 0);
         position: absolute;
-        bottom: -52%;
+        top: 86%;
     }
     .project-item:hover .info-overlay p {
         transition: all 3.5s ease-in-out;
@@ -201,7 +196,7 @@
         margin: .33rem 0;
         animation: lightText .5s ease-in-out forwards;
         overflow: hidden;
-        bottom: -52.5%;
+        top: 79%;
     }
     @keyframes lightText {
         0% {
@@ -228,37 +223,13 @@
 
     .project-item .actions a {
         color: #fff;
-        font-size: 1em;
-        margin: 0.5rem;
+        font-size: 1.2rem;
+        margin: 0 .5rem;
         transition: all 0.2s ease-out;
     }
     .project-item .actions a:hover {
         transition: all 0.2s ease-in-out;
         color: #782fee;
-    }
-
-    .btn.btn-miniDisplay {
-        background-color: #070707;
-        border-radius: .2rem;
-        line-height: 100%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        text-transform: uppercase;
-        padding: .5rem 1rem;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-        bottom: 4px;
-        position: relative;
-        width: 100%;
-        left: -8px;
-    }
-    .project-item:hover .btn.btn-miniDisplay {
-        color: #D2BEF2;
-        background-color: #782fee;
-        transition: all .1s ease-in;
-        box-shadow: none;
-        bottom: 9px;
-        border: none;
     }
 </style>
 
