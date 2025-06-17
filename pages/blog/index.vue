616f45f204970fd50b713a403bb240c033098163
Wellington N. <wellington.tvd@outlook.com.br>
2025-06-17 17:24:29 -0300

    feat: adicionar seção de blog com mensagem de reformulação em vários idiomas

---
+++ pages/blog/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Blog</h1>
+    <p>{{ t('html.blog.soon') }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+</script>
+
+<style scoped lang="scss">
+h1 {
+  font-size: 2rem;
+  margin-bottom: 1rem;
+}
+
+p {
+  font-size: 1.2rem;
+  color: var(--text-color, #333);
+}
+</style>
